79290f838b14754a79dc4c03969b5e6c6a101de4
emailproject019-beep <emailproject019@gmail.com>
2026-05-27 07:13:45 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,1 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    somnia: {
+      url: "https://rpc.shannon.somnia.network",
+      chainId: 50312,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
 
